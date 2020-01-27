33d98ed818987936bb3266e491ae236195b9b525
Mat Davidson <matdavidson88@gmail.com>
2020-01-27 11:13:24 -0500

    Added comments to the test suite for clarity

--- actors.test.js
+++ actors.test.js
@@ -3,15 +3,16 @@ Melee = require('./actors.js').Melee;
 Defender = require('./actors.js').Defender;
 Ranged = require('./actors').Ranged;
 
+//Test whether the base attacxk formula works as expected
 test('Base actor attack test', () => {
     let actor1 = new Actor('Bill');
     let actor2 = new Actor('Jim');
 
     actor1.attack(actor2);
-
     expect(actor2.hitPts).toBe(8);
 })
 
+//Test to ensure the Actor subclasses are properly created
 test('Melee values check', () => {
     let m = new Melee('Mike');
 
@@ -36,6 +37,7 @@ test('Ranged values check', () => {
     expect(m.attType).toContain('Ranged');
 })
 
+//Test to determine if the attack formula works when the arg actor is weak to this actor
 test('Modified attacked test', () => {
     let ran = new Ranged('Rick');
     let def = new Defender('Dan');
@@ -44,6 +46,7 @@ test('Modified attacked test', () => {
     expect(def.hitPts).toBe(6);  
 })
 
+//Test to determine if the attack formula works when the arg actor is resistant to this actor
 test('Modified resistance test', () => {
     let def = new Defender('Dan');
     let mel = new Melee('Mike');
