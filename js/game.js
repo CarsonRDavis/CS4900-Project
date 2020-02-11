59165e6ff72c1b458ea422decb7ae64c1ba9ae9c
Mat Davidson <matdavidson88@gmail.com>
2020-02-10 23:18:39 -0500

    Implemented LoadingManager

--- js/game.js
+++ js/game.js
@@ -9,26 +9,35 @@ var scene = new THREE.Scene;
 scene.background = new THREE.Color("#C0C0C0");
 //call method from worldGeneration.js
 boardGen(scene);
+const manager = new THREE.LoadingManager();
+
+
 //create camera and camera controls
 var camera = createCamera(width, height, renderer, scene);
 var controls = addCameraControls();
-
+createModels(manager);
 //add objects and set selectedCube
 //var selectedCube = addCubes();
 //createBanana();
-var def = new Defender('Dan');
-var mel = new Melee('Mike');
+// var def = new Defender('Dan');
+// var mel = new Melee('Mike');
+// var ran = new Range('Rick');
 
-def.model = createModel('defender', 'Dan', scene);
-mel.model = createModel('melee', 'Mike', scene);
-while(scene.getObjectByName(mel.model).position = undefined){
-    setTimeout(animate(), 50);
-    console.log("Timer");
+// def.model = createModel('defender', 'Dan');
+// mel.model = createModel('melee', 'Mike');
+// ran.model = createModel('ranged', 'Rick');
+
+function setPieces(){
+    scene.getObjectByName('melee').position.set(3.5, 0.01, -3.5);
+    scene.getObjectByName('ranged').position.set(8.5, 0.01, -3.5);
+    scene.getObjectByName('defender').position.set(3.5, 0.01, -3.5);
 }
-scene.getObjectByName(mel.model).position.set(3.5, 0.01, -3.5);
+
 
 //add event listeners
 window.addEventListener('keypress', cameraRotation, false);
 window.addEventListener('keypress', moveBanana, false);
 //call animate function
+
 animate();
+
--- js/modelMaker.js
+++ js/modelMaker.js
@@ -1,31 +1,66 @@
-function createModel(type, name, scene){
+function createModels(manager){
     // Creates and loads banana object with texture
-    var mMaterial = new THREE.MeshLambertMaterial({color:0x2194ce});
+    var redMat = new THREE.MeshLambertMaterial({color:0xF7573E});
+    var blueMat = new THREE.MeshLambertMaterial({color:0x2194ce});
+    var greenMat = new THREE.MeshLambertMaterial({color:0x11E020});
     //load the obj
-    var objLoader = new THREE.OBJLoader();
-    objLoader.load(
-        './models/' + type + '.obj',
-        function (object) {
-            object.traverse(function(node){
-                if(node.isMesh){
-                    node.material = mMaterial;
-                    if(type === 'defender'){
-                        node.scale.set(.25, .25, .25);
-                    }
-                    else
-                        node.scale.set(.25, .25, .25);
-                }
-            });
-            object.position.set(0.5, 0.01, -3.5);
-            object.rotation.x -= Math.PI / 2;
-            object.name = name;
-            scene.add(object);
+    
+    const models = {
+      melee:    { url: './models/melee.obj', name: 'melee', material: redMat, pos: 3.5 },
+      ranged:   { url: './models/ranged.obj', name: 'ranged', material: blueMat, pos: 1.75 },
+      defender: { url: './models/defender.obj', name: 'defender', material: greenMat, pos: 0.5 },
+    };
+    const objLoader = new THREE.OBJLoader(manager);
+
+    for (const model of Object.values(models)) {
+      objLoader.load(model.url, function (object) {
+        object.traverse(function(node){
+          if(node.isMesh){
+            node.material = model.material;                        
+            node.scale.set(.25, .25, .25);
+          }
+        });
+        object.position.set(model.pos, 0.01, -3.5);
+        object.rotation.x -= Math.PI / 2;
+        object.name = model.name;
+        scene.add(object);
         }
-    );
-        animate();
-    return name;
+      );
+    }
+    //manager.onLoad = setPieces();
 }
 
+// const manager = new THREE.LoadingManager();
+// manager.onLoad = init;
+// const models = {
+//   melee:    { url: './models/melee.obj', name: 'melee' },
+//   ranged:   { url: './models/ranged.obj', name: 'ranged' },
+//   defender: { url: './models/defender.obj', name: 'defender' },
+// };
+// {
+//   const objLoader = new THREE.OBJLoader(manager);
+//   for (const model of Object.values(models)) {
+//     objLoader.load(model.url, function (object) {
+//             object.traverse(function(node){
+//                 if(node.isMesh){
+//                     node.material = mMaterial;                        
+//                     node.scale.set(.25, .25, .25);
+//                 }
+//             });
+//             object.position.set(0.5, 0.01, -3.5);
+//             object.rotation.x -= Math.PI / 2;
+//             object.name = name;
+//             scene.add(object);
+//         }
+//     );
+//   }
+// }
+ 
+// function init() {
+//   // TBD
+// }
+
+//Model sources
 //https://free3d.com/3d-model/fig-winterrangerlongbow-v1--418439.html
 //https://free3d.com/3d-model/viking-warriors-swordand-shield-v1--284280.html
 //https://free3d.com/3d-model/amazonwarrior-spear-and-shield-v1--685137.html
