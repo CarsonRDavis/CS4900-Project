bc7394dd54828d5c9fdddea756a57272c5358f18
Emily Shirley <emarie12816@gmail.com>
2020-02-18 13:24:09 -0500

    adding Carson's minor camera changes

--- js/camera.js
+++ js/camera.js
@@ -1,17 +1,17 @@
 //This file creates the camera for the scene
-function createCamera (width, height, renderer, scene) {
+function createCamera(width, height, renderer, scene) {
     //create camera
     var camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 10000);
     //set position
     camera.position.set(0, 7, -5);
-    
+
     //render
     renderer.render(scene, camera);
-    
+
     return camera;
 }
 
-function addCameraControls(camera, renderer){
+function addCameraControls(camera, renderer) {
     // Creates an OrbitControls object to be able to rotate the camera around an object
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.name = "controls";
@@ -20,10 +20,19 @@ function addCameraControls(camera, renderer){
         LEFT: 81,
         RIGHT: 69
     };
-    
+
     controls.update();
-    
-    return controls;
+
+    controls.maxPolarAngle = Math.PI / 2.5;
+    controls.minPolarAngle = Math.PI / 5;
+
+    controls.minDistance = 3;
+    controls.maxDistance = 10;
+
+    return controls
 }
 
-export { createCamera, addCameraControls };
+export {
+    createCamera,
+    addCameraControls
+};
