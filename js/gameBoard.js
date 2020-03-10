8d92f270b2591ed6ebd405c5b415e041180c454a
Mat Davidson <matdavidson88@gmail.com>
2020-03-10 13:40:39 -0400

    Added random placement functionality to boardGen

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -60,27 +60,44 @@ function boardGen(scene, heightMap) {
         let caster = new THREE.Raycaster(new THREE.Vector3(0,0,0), down);
         caster.far = 0.05;
         let unit = mapVerts/(mapVerts - 1);
-        let x = unit/2;
+        let mid = (mapVerts - 1)/2;
         console.log(numObjs);
 
-        const models = {
-            bush:    { url: './models/Bush_2/Bush_2.gltf'},
-            berryBush:   { url: './models/BushBerries_1/BushBerries_1.gltf'},
-            commonTree: { url: './models/CommonTree_2/CommonTree_2.gltf'},
-            commonTree2: { url: './models/CommonTree_3/CommonTree_3.gltf'},
-            pineTree: { url: './models/PineTree_5/PineTree_5.gltf'},
-            rock:    { url: './models/Rock_1/Rock_1.gltf'},
-            rock2:    { url: './models/Rock_2/Rock_2.gltf'},
-          };
+        const models = [
+          { url: './models/Bush_2/Bush_2.gltf', scale: 0.75},
+          { url: './models/BushBerries_1/BushBerries_1.gltf', scale: 0.75},
+          { url: './models/CommonTree_2/CommonTree_2.gltf', scale: 0.75},
+          { url: './models/CommonTree_3/CommonTree_3.gltf', scale: 0.75},
+          { url: './models/PineTree_5/PineTree_5.gltf', scale: 0.75},
+          { url: './models/Rock_1/Rock_1.gltf', scale: 0.75},
+          { url: './models/Rock_2/Rock_2.gltf', scale: 0.75},
+        ];
+          //console.log(models[0].url);
           const gltfLoader = new THREE.GLTFLoader(manager);
-          for (const model of Object.values(models)){
-            gltfLoader.load(model.url, (gltf) => {
+          // for (const model of Object.values(models)){
+          for(let i = 0; i<numObjs; i++){
+            let choice = getRandomInt(models.length)
+            gltfLoader.load(models[choice].url, (gltf) => {
               const root = gltf.scene;
-              
-              root.position.set(-5*unit + x, 0.01, unit/2);     
-              x += 2*unit;        
+              let x = getRandomInt(mid);
+              let y = getRandomInt(mid);
+              let quad = getRandomInt(4);
+
+              if(quad == 0){
+                root.position.set((unit/2) + x*unit, 0.01, (unit/2) + y*unit);
+              }
+              else if(quad == 1){
+                root.position.set((unit/2) + x*unit, 0.01, -(unit/2) - y*unit);
+              }
+              else if(quad ==2){
+                root.position.set(-(unit/2) - x*unit, 0.01, -(unit/2) - y*unit);
+              }
+              else{
+                root.position.set(-(unit/2) - x*unit, 0.01, (unit/2) + y*unit);
+              }
+                          
               root.rotation.y += Math.PI;
-              root.scale.set(.75,.75,.75)
+              root.scale.set(models[choice].scale,models[choice].scale,models[choice].scale)
       
               caster.set(root.position, down);
               let intersects = caster.intersectObjects(scene.children);
@@ -91,6 +108,7 @@ function boardGen(scene, heightMap) {
                 intersects = caster.intersectObjects(scene.children);
               }
               root.position.y += .95;
+              terObjs.push(root);
               scene.add(root);              
             });
         }    
@@ -99,3 +117,6 @@ function boardGen(scene, heightMap) {
 
 export{boardGen};
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
